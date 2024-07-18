33178ccb40cb454ba21274fb522ceb369fba57f0
Franklin Bynum <franklin@bynumlaw.net>
2024-07-18 11:22:38 -0500

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,23 +1,22 @@
 local wezterm = require("wezterm")
 
 function scheme_for_appearance(appearance)
-	if appearance:find 'Dark' then
-	  return 'midnight-in-mojave'
+	if appearance:find("Dark") then
+		return "midnight-in-mojave"
 	else
-	  return 'Mostly Bright (terminal.sexy)'
+		return "Mostly Bright (terminal.sexy)"
 	end
-  end
-  
-  wezterm.on('window-config-reloaded', function(window, pane)
+end
+
+wezterm.on("window-config-reloaded", function(window, pane)
 	local overrides = window:get_config_overrides() or {}
 	local appearance = window:get_appearance()
 	local scheme = scheme_for_appearance(appearance)
 	if overrides.color_scheme ~= scheme then
-	  overrides.color_scheme = scheme
-	  window:set_config_overrides(overrides)
+		overrides.color_scheme = scheme
+		window:set_config_overrides(overrides)
 	end
-  end)
-  
+end)
 
 return {
 
