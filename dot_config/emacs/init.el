be6bfbf93ddc2df10f0733821f35f2010f12dfd3
Franklin Bynum <franklin@bynumlaw.net>
2025-04-12 14:11:46 -0500

    Update .config/emacs/init.el

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -166,7 +166,7 @@
 (use-package envrc
   :hook (after-init . envrc-global-mode))
 
-;; Eglot Config 
+;; Eglot Config
 (add-hook 'prog-mode-hook #'eglot-ensure)
 (with-eval-after-load 'eglot
   (setq eglot-autoshutdown t)
@@ -178,7 +178,10 @@
   (add-to-list 'eglot-server-programs
                '(text-mode . ("harper-ls" "--stdio")))
   (add-to-list 'eglot-server-programs
-               '(markdown-mode . ("harper-ls" "--stdio"))))
+               '(markdown-mode . ("harper-ls" "--stdio")))
+    (add-to-list 'eglot-server-programs
+               '(mail-mode . ("harper-ls" "--stdio"))))
+
 
 (setq-default eglot-workspace-configuration
               '(:harper-ls (:userDictPath ""
@@ -231,80 +234,34 @@
 (use-package treemacs
   :ensure t
   :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
   :config
   (progn
-    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
-          treemacs-deferred-git-apply-delay        0.5
-          treemacs-directory-name-transformer      #'identity
-          treemacs-display-in-side-window          t
-          treemacs-eldoc-display                   'simple
-          treemacs-file-event-delay                2000
-          treemacs-file-extension-regex            treemacs-last-period-regex-value
-          treemacs-file-follow-delay               0.2
-          treemacs-file-name-transformer           #'identity
-          treemacs-follow-after-init               t
-          treemacs-expand-after-init               t
-          treemacs-find-workspace-method           'find-for-file-or-pick-first
-          treemacs-git-command-pipe                ""
-          treemacs-goto-tag-strategy               'refetch-index
-          treemacs-header-scroll-indicators        '(nil . "^^^^^^")
-          treemacs-hide-dot-git-directory          t
-          treemacs-indentation                     2
-          treemacs-indentation-string              " "
-          treemacs-is-never-other-window           nil
-          treemacs-max-git-entries                 5000
-          treemacs-missing-project-action          'ask
-          treemacs-move-files-by-mouse-dragging    t
-          treemacs-move-forward-on-expand          nil
-          treemacs-no-png-images                   nil
-          treemacs-no-delete-other-windows         t
-          treemacs-project-follow-cleanup          nil
-          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-position                        'left
-          treemacs-read-string-input               'from-child-frame
-          treemacs-recenter-distance               0.1
-          treemacs-recenter-after-file-follow      nil
-          treemacs-recenter-after-tag-follow       nil
-          treemacs-recenter-after-project-jump     'always
-          treemacs-recenter-after-project-expand   'on-distance
-          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
-          treemacs-project-follow-into-home        nil
-          treemacs-show-cursor                     nil
-          treemacs-show-hidden-files               t
-          treemacs-silent-filewatch                nil
-          treemacs-silent-refresh                  nil
-          treemacs-sorting                         'alphabetic-asc
-          treemacs-select-when-already-in-treemacs 'move-back
-          treemacs-space-between-root-nodes        t
-          treemacs-tag-follow-cleanup              t
-          treemacs-tag-follow-delay                1.5
-          treemacs-text-scale                      nil
-          treemacs-user-mode-line-format           nil
-          treemacs-user-header-line-format         nil
-          treemacs-wide-toggle-width               70
-          treemacs-width                           35
-          treemacs-width-increment                 1
-          treemacs-width-is-initially-locked       t
-          treemacs-workspace-switch-cleanup        nil)
-
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    ;;(treemacs-resize-icons 44)
-
+    ;; Only keep essential customizations
+    (setq treemacs-width 35
+          treemacs-position 'left
+          treemacs-show-hidden-files t
+          treemacs-litter-directories '("/node_modules" "/.venv" "/.cask"))
+    
+    ;; Enable standard modes
     (treemacs-follow-mode t)
     (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode 'always)
-    (when treemacs-python-executable
-      (treemacs-git-commit-diff-mode t))
-
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null treemacs-python-executable)))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple)))
-
-    (treemacs-hide-gitignored-files-mode nil)))
+    (treemacs-fringe-indicator-mode t)
+    
+    ;; Simple git integration if available
+    (when (executable-find "git")
+      (treemacs-git-mode 'simple)))
+    :bind
+    (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t d"   . treemacs-select-directory)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
 
 (use-package treemacs-icons-dired
   :hook (dired-mode . treemacs-icons-dired-enable-once)
@@ -318,6 +275,34 @@
   :after treemacs
   :config
   (treemacs-load-theme "nerd-icons"))
+
+;;;; Org Mode Configuration
+
+(use-package org
+  :config
+  (setq org-startup-indented t        ; Enable org-indent-mode by default
+        org-hide-emphasis-markers t   ; Hide formatting characters
+        org-pretty-entities t         ; Show UTF8 characters
+        org-startup-folded 'content)  ; Start with top-level headings unfolded
+  :hook
+  (org-mode . visual-line-mode))      ; Better line wrapping
+
+;; Better bullet points
+(use-package org-bullets
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+;; Better org structure display
+(setq org-ellipsis " ▾"               ; Symbol to show folded content
+      org-hide-leading-stars t)       ; Hide leading stars
+
+;; Simple org agenda setup
+(setq org-agenda-files '("~/org")
+      org-agenda-window-setup 'current-window
+      org-agenda-span 10
+      org-agenda-start-on-weekday nil
+      org-log-done 'time)
 
 ;;;; Language-Specific Configuration
 
